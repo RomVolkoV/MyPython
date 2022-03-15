af4d1d9fe487e12b3cd3dbc6e92e6740c0c28cb2
ROMV <romv@live.ru>
2022-03-15 12:04:41 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	alpha.py

--- alpha.py
+++ alpha.py
@@ -1,7 +1,14 @@
-#Получаем алфавит
-alphabet = [chr(i) for i in range(65, 91)] + [chr(j) for j in range(97, 123)]
-print(alphabet)
-h1,m1,s1,h2,m2,s2 = [int(input()) for i in 'aaaaaa']
-print(h1,m1,s1,h2,m2,s2)
-h1,m1,s1,h2,m2,s2 = [int(input()) for _ in range(6)]
-print(h1,m1,s1,h2,m2,s2)
+# Получаем алфавит
+# alphabet = [chr(i) for i in range(65, 91)] + [chr(j) for j in range(97, 123)]
+# print(alphabet)
+# h1,m1,s1,h2,m2,s2 = [int(input()) for i in 'aaaaaa']
+# print(h1,m1,s1,h2,m2,s2)
+# h1,m1,s1,h2,m2,s2 = [int(input()) for _ in range(6)]
+# print(h1,m1,s1,h2,m2,s2)
+
+# c = [x ** 3 for x in range(20) if x%2==1]
+# print(c)
+
+b1 = True
+b2 = True
+print(b1 and b2)
